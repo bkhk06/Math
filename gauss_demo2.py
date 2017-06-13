0e15cbad81df681606b8ce2f0af44c018176810e
bkhk06 <bkhk06@users.noreply.github.com>
2017-06-13 16:09:22 +0800

    Create gauss_demo2.py

--- gauss_demo2.py
+++ gauss_demo2.py
@@ -7,7 +7,7 @@ ax = Axes3D(fig)
 rv = stats.multivariate_normal([0, 0], cov=1)
 
 x, y = np.mgrid[-3:3:.15, -3:3:.15]
-ax.plot_surface(x, y, rv.pdf(np.dstack((x, y))), rstride=1, cstride=1)
+ax.plot_surface(x, y, rv.pdf(np.dstack((x, y))), rstride=1, cstride=1,cmap='rainbow')
 ax.set_zlim(0, 0.2)
 
 # savefig('../figures/plot3d_ex.png',dpi=48)
